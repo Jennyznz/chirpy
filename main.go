01aa845c527cbaa6ca238698175a2879e7ee35fe
Jenny <156132099+Jennyznz@users.noreply.github.com>
2026-06-11 18:14:18 -0400

    Add handlers to track to number of requests made on the server

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"sync/atomic"
+	"fmt"
+)
 
 func main() {
 	serveMux := http.NewServeMux()
 	fullDir := http.Dir(".")
-	serveMux.Handle("/", http.FileServer(fullDir))
+	strippedHandler := http.StripPrefix("/app", http.FileServer(fullDir))
+	apiConfig_1 := &apiConfig{}
+	
+	serveMux.Handle("/app/", apiConfig_1.middlewareMetricsInc(strippedHandler))
+	serveMux.HandleFunc("/healthz", readinessEndpoints)
+	serveMux.HandleFunc("/metrics", apiConfig_1.getNumHits)
+	serveMux.HandleFunc("/reset", apiConfig_1.resetHits)
 
 	server := &http.Server {
 		Handler: serveMux,
@@ -13,6 +23,32 @@ func main() {
 	}
 
 	server.ListenAndServe()
+}
+
+func readinessEndpoints(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {	// Handler as parameter and return statement because middleware
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {	// Runs every time after the first call to middlewareMetricsInc
+		cfg.fileserverHits.Add(int32(1))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getNumHits(w http.ResponseWriter, r *http.Request) {	// http.ServeMux needs every handler to accept a http writer and reader, even if not used
+	result := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	w.Write([]byte(result))
+}
+
+func(cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request) { 
+	cfg.fileserverHits.Store(0); 
+}
+
+type apiConfig struct {
+	fileserverHits atomic.Int32	// stdlib that safety increments and reads ints across goroutines
 }
 
 
